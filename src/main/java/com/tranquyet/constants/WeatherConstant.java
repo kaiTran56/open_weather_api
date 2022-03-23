7026e04edf334b4de0063fde56a53114325cd54d
tranquyet <quyettran057@gmail.com>
2022-03-23 22:25:13 +0700

    updated db

--- src/main/java/com/tranquyet/constants/WeatherConstant.java
+++ src/main/java/com/tranquyet/constants/WeatherConstant.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum WeatherConstant {
-	CLEAR_SKY("clear sky", "https://img.locationscout.net/images/2019-09/tree-in-the-sunset-germany_l.jpeg"),
-	CLOUDS("clouds", "https://img.locationscout.net/images/2019-09/tree-in-the-sunset-germany_l.jpeg"),
-	RAIN("rain", "https://img.locationscout.net/images/2019-09/tree-in-the-sunset-germany_l.jpeg"),
-	THUNDERSTORM("thunderstorm", "https://img.locationscout.net/images/2019-09/tree-in-the-sunset-germany_l.jpeg"),
-	SNOW("snow", "https://img.locationscout.net/images/2019-09/tree-in-the-sunset-germany_l.jpeg"),
-	MIST("mist", "https://img.locationscout.net/images/2019-09/tree-in-the-sunset-germany_l.jpeg"),
+	CLEAR_SKY("clear sky",
+			"https://i.tribune.com.pk/media/images/5-59555_photo-wallpaper-the-sun-dawn-coffee-mornin1626689182-0/5-59555_photo-wallpaper-the-sun-dawn-coffee-mornin1626689182-0.jpg"),
+	CLOUDS("clouds", "https://cloudappreciationsociety.org/wp-content/uploads/2017/03/Jen-M.jpg"),
+	RAIN("rain", "https://c4.wallpaperflare.com/wallpaper/291/944/37/rain-dark-water-drops-wallpaper-preview.jpg"),
+	THUNDERSTORM("thunderstorm",
+			"https://images.unsplash.com/photo-1594760467013-64ac2b80b7d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80"),
+	SNOW("snow", "https://www.sciencenewsforstudents.org/wp-content/uploads/2021/02/1030_LL_snow.jpg"),
+	MIST("mist", "https://www.caa.gov.qa/en-us/CAA%20Images/English%20news/En-Caa%20news/fog%203932.jpg"),
 
 	CLEAR_SKY_ICON("clear sky", "https://icons.iconarchive.com/icons/pixelkit/flat-jewels/512/Weather-icon.png"),
 	CLOUDS_ICON("clouds",
