212edb865cd441fa409509ebdd4c7b3299dec8c6
tranquyet <quyettran057@gmail.com>
2022-03-23 23:46:49 +0700

    deploy server

--- src/main/java/com/tranquyet/config/PropertiesConfig.java
+++ src/main/java/com/tranquyet/config/PropertiesConfig.java
@@ -1,11 +1,7 @@
 package com.tranquyet.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-
-@Configuration
-@PropertySources({ @PropertySource("classpath:cities.properties") })
-public class PropertiesConfig {
-
-}
+//@Configuration
+//@PropertySources({ @PropertySource("classpath:cities.properties") })
+//public class PropertiesConfig {
+//
+//}
