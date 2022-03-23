0da408d2de65f363cc16e6f6d8069616f6b4f2d3
tranquyet <quyettran057@gmail.com>
2022-03-23 23:19:41 +0700

    deploy server

--- src/main/java/com/tranquyet/OpenWeatherApiApplication.java
+++ src/main/java/com/tranquyet/OpenWeatherApiApplication.java
@@ -3,10 +3,8 @@ package com.tranquyet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableJpaAuditing
 public class OpenWeatherApiApplication {
 
 	public static void main(String[] args) {
